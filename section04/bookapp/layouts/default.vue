f051b4203ef1912b8c794d7ac67fcaac127485b5
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-02-19 17:36:23 +0900

    55 section04フォルダの準備

---
+++ section04/bookapp/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <Header />
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {}
+  },
+}
+</script>
